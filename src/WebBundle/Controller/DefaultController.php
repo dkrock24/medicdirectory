c58f4ebf04ea6a0b22cd7e82bb86a55d4209f2bd
Carlos Hidalgo <vladimiroski@gmail.com>
2016-10-03 17:43:11 -0600

    Avances de Viernes 30

--- src/WebBundle/Controller/DefaultController.php
+++ src/WebBundle/Controller/DefaultController.php
@@ -8,6 +8,26 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('WebBundle:Default:index.html.twig');
+        return $this->render('WebBundle:Sections:index.html.twig');
+    }
+    
+    public function indexDoctoresAction()
+    {
+        return $this->render('WebBundle:Doctores:index.html.twig');
+    }
+    
+    public function indexProfileAction()
+    {
+        return $this->render('WebBundle:Doctores:profile.html.twig');
+    }
+ 
+    public function indexHospitalesAction()
+    {
+        return $this->render('WebBundle:Hospitales:index.html.twig');
+    }
+
+    public function indexFarmaciasAction()
+    {
+        return $this->render('WebBundle:Farmacias:index.html.twig');
     }
 }
