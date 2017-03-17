857252a558580be57ef5fdb274c2994a4c9106e0
Vladimir Hidalgo <vladimiroski@gmail.com>
2017-03-16 22:07:07 -0600

    servicioParametros

---
+++ src/AppBundle/Services/servicioParametros.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Services;
+
+/**
+ * Description of servicioParametros
+ *
+ * @author vladimir
+ */
+
+use \AppBundle\Entity\Parametros;
+
+class servicioParametros {
+    /* @var $em \Doctrine\ORM\EntityManager */
+    private $em;
+
+    function __construct($em) {
+        $this->em = $em;
+    }
+
+    public function setParametro($llave, $valor) {
+        $this->em->getRepository(Parametros::class)->findOneBy(array('parLlave' => $llave));
+
+    }
+
+    public function getParametro($llave, $valor = '') {
+        /* @var $parametro \AppBundle\Entity\Parametros */
+        $parametro = $this->em->getRepository(Parametros::class)->findOneBy(array('parLlave' => $llave));
+
+        return $parametro->getParValor();
+
+    }
+
+}
