19d88f564108b05cf7a5fc14dab8b56d1de7fc55
gio <gialvarezlopez@gmail.com>
2017-03-24 13:21:22 +0800

    CRUD departamentos

--- src/AdminBundle/Controller/DepartamentoController.php
+++ src/AdminBundle/Controller/DepartamentoController.php
@@ -5,6 +5,7 @@ namespace AdminBundle\Controller;
 use AppBundle\Entity\Departamento;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 /**
  * Departamento controller.
@@ -12,6 +13,12 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class DepartamentoController extends Controller
 {
+	
+	private $session;
+	
+	public function __construct() {
+		$this->session = new Session();
+	}
     /**
      * Lists all departamento entities.
      *
@@ -41,7 +48,18 @@ class DepartamentoController extends Controller
             $em = $this->getDoctrine()->getManager();
 			$departamento->setDepFechaCrea( new \DateTime() );
             $em->persist($departamento);
-            $em->flush($departamento);
+            $flush = $em->flush($departamento);
+			
+			if ($flush == null)
+			{
+				$msgBox = "Registro creado con éxito";
+				$status = "success";
+			} else {
+				$msgBox = "No se pudo crear el registro ";
+				$status = "error";
+			}
+			
+			$this->session->getFlashBag()->add($status,$msgBox);
 
             return $this->redirectToRoute('departamento_show', array('id' => $departamento->getDepId()));
         }
@@ -73,16 +91,36 @@ class DepartamentoController extends Controller
     public function editAction(Request $request, Departamento $departamento)
     {
         $deleteForm = $this->createDeleteForm($departamento);
-        $editForm = $this->createForm('AppBundle\Form\DepartamentoType', $departamento);
+        $editForm = $this->createForm('AdminBundle\Form\DepartamentoType', $departamento);
         $editForm->handleRequest($request);
 
         if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+			
+            $em = $this->getDoctrine()->getManager();
+			
+			
+			//Update field dep_fecha_mod
+			$id = $editForm->getData()->getDepId();
+			$item = $em->getRepository('AppBundle:Departamento')->find($id);
+			$item->setDepFechaMod(new \DateTime());
+			//end 
+			$flush = $em->flush($item);
+			
+			if ($flush == null)
+			{
+				$msgBox = "Registro actualizado con éxito";
+				$status = "success";
+			} else {
+				$msgBox = "No se ha podido actualizar el registro ";
+				$status = "error";
+			}
+			
+			$this->session->getFlashBag()->add($status,$msgBox);
 
-            return $this->redirectToRoute('departamento_edit', array('id' => $departamento->getId()));
+            return $this->redirectToRoute('departamento_edit', array('id' => $departamento->getDepId()));
         }
 
-        return $this->render('departamento/edit.html.twig', array(
+        return $this->render('AdminBundle:departamento:edit.html.twig', array(
             'departamento' => $departamento,
             'edit_form' => $editForm->createView(),
             'delete_form' => $deleteForm->createView(),
@@ -101,11 +139,56 @@ class DepartamentoController extends Controller
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->remove($departamento);
-            $em->flush();
+			
+			$flush = $em->flush();
+			
+			if ($flush == null)
+			{
+				$msgBox = "El registro fue eliminado con éxito";
+				$status = "success";
+			} else {
+				$msgBox = "No se ha podido eliminar el registro ";
+				$status = "error";
+			}
+			
+			$this->session->getFlashBag()->add($status,$msgBox);
         }
 
         return $this->redirectToRoute('departamento_index');
     }
+	
+	
+	public function deleteCustomAction( Request $request )
+	{
+		$iId = $request->request->get('id');
+		//$iId = $request->query->get('id');
+		
+		if( isset($iId) && $iId > 0 )
+		{
+			
+			try
+			{
+				$em = $this->getDoctrine()->getManager();
+				$repo = $em->getRepository("AppBundle:Departamento");	
+				$item = $repo->find($iId);
+				$em->remove($item);
+				$flush = $em->flush();
+
+				if ($flush == null)
+				{
+					echo 1;
+				} else {
+					echo 0;
+				}
+				
+			}catch (\Exception $e){
+				echo ($e->getMessage());
+			}
+			
+		}
+		
+		exit();
+	}
 
     /**
      * Creates a form to delete a departamento entity.
