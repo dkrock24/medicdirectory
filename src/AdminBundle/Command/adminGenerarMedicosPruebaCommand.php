f3faab43e6013ce02304a93fb85d1ad1539aa1a3
Vladimir Hidalgo <vladimiroski@gmail.com>
2017-04-01 18:18:42 -0600

    DataFixtures

--- src/AdminBundle/Command/adminGenerarMedicosPruebaCommand.php
+++ src/AdminBundle/Command/adminGenerarMedicosPruebaCommand.php
@@ -2,13 +2,18 @@
 
 namespace AdminBundle\Command;
 
-use Symfony\Component\Console\Command\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 
-class adminGenerarMedicosPruebaCommand extends Command {
+class adminGenerarMedicosPruebaCommand extends ContainerAwareCommand {
+
+    /* @var $em \Doctrine\ORM\EntityManager */
+    private $em = null;
 
     protected function configure() {
+        /* php bin/console admin:generar-medicos-prueba */
         $this
                 // the name of the command (the part after "bin/console")
                 ->setName('admin:generar-medicos-prueba')
@@ -19,11 +24,13 @@ class adminGenerarMedicosPruebaCommand extends Command {
                 // the full command description shown when running the command with
                 // the "--help" option
                 ->setHelp("Crea medicos de manera aleatoria con un numero definido por el primer parametro")
+                ->addArgument('cantidad', InputArgument::REQUIRED, '¿Cuantos médicos?')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
-        $num = 0;
+        $this->em = $this->getContainer()->get('doctrine')->getManager();
+        $iCantidad = $input->getArgument('cantidad');
 
         $output->writeln([
             '',
@@ -31,8 +38,11 @@ class adminGenerarMedicosPruebaCommand extends Command {
             'Creador de medicos',
             '========================',
             '',
-            'Medicos a crear: ' . $num,
+            'Medicos a crear: ' . $iCantidad,
         ]);
+
+        
+
     }
 
 }
