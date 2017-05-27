fefa84f704c09b4c7414a2a56f3ea94f94fb8eec
Vladimir Hidalgo <vladimiroski@gmail.com>
2017-05-26 22:05:58 -0600

    Entities faltantes

---
+++ src/AppBundle/Entity/ClienteGaleria.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * ClienteGaleria
+ */
+class ClienteGaleria
+{
+    /**
+     * @var integer
+     */
+    private $cliGalId;
+
+    /**
+     * @var string
+     */
+    private $cliGalHash;
+
+    /**
+     * @var string
+     */
+    private $cliGalTipo;
+
+    /**
+     * @var \DateTime
+     */
+    private $cliGalFechaCrea = 'CURRENT_TIMESTAMP';
+
+    /**
+     * @var \DateTime
+     */
+    private $cliGalFechaMod;
+
+    /**
+     * @var boolean
+     */
+    private $cliGalActivo = '1';
+
+    /**
+     * @var \AppBundle\Entity\Cliente
+     */
+    private $galCliente;
+
+
+    /**
+     * Get cliGalId
+     *
+     * @return integer
+     */
+    public function getCliGalId()
+    {
+        return $this->cliGalId;
+    }
+
+    /**
+     * Set cliGalHash
+     *
+     * @param string $cliGalHash
+     *
+     * @return ClienteGaleria
+     */
+    public function setCliGalHash($cliGalHash)
+    {
+        $this->cliGalHash = $cliGalHash;
+
+        return $this;
+    }
+
+    /**
+     * Get cliGalHash
+     *
+     * @return string
+     */
+    public function getCliGalHash()
+    {
+        return $this->cliGalHash;
+    }
+
+    /**
+     * Set cliGalTipo
+     *
+     * @param string $cliGalTipo
+     *
+     * @return ClienteGaleria
+     */
+    public function setCliGalTipo($cliGalTipo)
+    {
+        $this->cliGalTipo = $cliGalTipo;
+
+        return $this;
+    }
+
+    /**
+     * Get cliGalTipo
+     *
+     * @return string
+     */
+    public function getCliGalTipo()
+    {
+        return $this->cliGalTipo;
+    }
+
+    /**
+     * Set cliGalFechaCrea
+     *
+     * @param \DateTime $cliGalFechaCrea
+     *
+     * @return ClienteGaleria
+     */
+    public function setCliGalFechaCrea($cliGalFechaCrea)
+    {
+        $this->cliGalFechaCrea = $cliGalFechaCrea;
+
+        return $this;
+    }
+
+    /**
+     * Get cliGalFechaCrea
+     *
+     * @return \DateTime
+     */
+    public function getCliGalFechaCrea()
+    {
+        return $this->cliGalFechaCrea;
+    }
+
+    /**
+     * Set cliGalFechaMod
+     *
+     * @param \DateTime $cliGalFechaMod
+     *
+     * @return ClienteGaleria
+     */
+    public function setCliGalFechaMod($cliGalFechaMod)
+    {
+        $this->cliGalFechaMod = $cliGalFechaMod;
+
+        return $this;
+    }
+
+    /**
+     * Get cliGalFechaMod
+     *
+     * @return \DateTime
+     */
+    public function getCliGalFechaMod()
+    {
+        return $this->cliGalFechaMod;
+    }
+
+    /**
+     * Set cliGalActivo
+     *
+     * @param boolean $cliGalActivo
+     *
+     * @return ClienteGaleria
+     */
+    public function setCliGalActivo($cliGalActivo)
+    {
+        $this->cliGalActivo = $cliGalActivo;
+
+        return $this;
+    }
+
+    /**
+     * Get cliGalActivo
+     *
+     * @return boolean
+     */
+    public function getCliGalActivo()
+    {
+        return $this->cliGalActivo;
+    }
+
+    /**
+     * Set galCliente
+     *
+     * @param \AppBundle\Entity\Cliente $galCliente
+     *
+     * @return ClienteGaleria
+     */
+    public function setGalCliente(\AppBundle\Entity\Cliente $galCliente = null)
+    {
+        $this->galCliente = $galCliente;
+
+        return $this;
+    }
+
+    /**
+     * Get galCliente
+     *
+     * @return \AppBundle\Entity\Cliente
+     */
+    public function getGalCliente()
+    {
+        return $this->galCliente;
+    }
+}
---
+++ src/AppBundle/Entity/UsuarioVistas.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * UsuarioVistas
+ */
+class UsuarioVistas
+{
+    /**
+     * @var integer
+     */
+    private $visId;
+
+    /**
+     * @var string
+     */
+    private $visReferencia;
+
+    /**
+     * @var \DateTime
+     */
+    private $visFechaCrea = 'CURRENT_TIMESTAMP';
+
+    /**
+     * @var \AppBundle\Entity\Usuario
+     */
+    private $visUsuario;
+
+
+    /**
+     * Get visId
+     *
+     * @return integer
+     */
+    public function getVisId()
+    {
+        return $this->visId;
+    }
+
+    /**
+     * Set visReferencia
+     *
+     * @param string $visReferencia
+     *
+     * @return UsuarioVistas
+     */
+    public function setVisReferencia($visReferencia)
+    {
+        $this->visReferencia = $visReferencia;
+
+        return $this;
+    }
+
+    /**
+     * Get visReferencia
+     *
+     * @return string
+     */
+    public function getVisReferencia()
+    {
+        return $this->visReferencia;
+    }
+
+    /**
+     * Set visFechaCrea
+     *
+     * @param \DateTime $visFechaCrea
+     *
+     * @return UsuarioVistas
+     */
+    public function setVisFechaCrea($visFechaCrea)
+    {
+        $this->visFechaCrea = $visFechaCrea;
+
+        return $this;
+    }
+
+    /**
+     * Get visFechaCrea
+     *
+     * @return \DateTime
+     */
+    public function getVisFechaCrea()
+    {
+        return $this->visFechaCrea;
+    }
+
+    /**
+     * Set visUsuario
+     *
+     * @param \AppBundle\Entity\Usuario $visUsuario
+     *
+     * @return UsuarioVistas
+     */
+    public function setVisUsuario(\AppBundle\Entity\Usuario $visUsuario = null)
+    {
+        $this->visUsuario = $visUsuario;
+
+        return $this;
+    }
+
+    /**
+     * Get visUsuario
+     *
+     * @return \AppBundle\Entity\Usuario
+     */
+    public function getVisUsuario()
+    {
+        return $this->visUsuario;
+    }
+}
