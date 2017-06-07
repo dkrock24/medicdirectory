2fa5caeb577aa6c5f834b4a84f32639c13be2b72
Carlos Hidalgo <vladimiroski@gmail.com>
2017-06-06 18:36:58 -0600

    UsuarioSocial

--- implementar_en_linux.sh
+++ implementar_en_linux.sh
@@ -3,6 +3,7 @@ git pull -f origin master && \
 composer.phar install && \
 php bin/console assets:install --symlink && \
 php bin/console doctrine:schema:update --force --dump-sql && \
+php bin/console cache:clear --env=dev && \
 php bin/console cache:clear --env=prod && \
 chown -R www-data:www-data web/uploads && \
 chmod -R og+srw web/uploads
