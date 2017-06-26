898b7666c6fc866f37161ad10aebcb1abca7e22d
gio <gialvarezlopez@gmail.com>
2017-06-26 11:59:23 +0800

    AJUSTES CON ESTABLECIMIENTOS

--- src/EmrBundle/Controller/DashboardController.php
+++ src/EmrBundle/Controller/DashboardController.php
@@ -49,25 +49,30 @@ class DashboardController extends Controller
             //4 = Receso
             //5 = Otros
 			//-----------------------------------
+			if( $locationId != "" )
+			{
 			
-			$start = date("Y-m-d");
-			$end = date("Y-m-d");
-			$filter = " AND a.age_usu_id = " . $idUser;
-			$RAW_QUERY = "SELECT a.age_id as id, a.age_fecha_inicio as start, a.age_fecha_fin as end, a.age_tipo_evento as tipo_evento,  p.pac_nombre, p.pac_seg_nombre, p.pac_apellido, p.pac_seg_apellido,p.pac_dui
-								FROM agenda a
-								LEFT JOIN cita c on c.cit_id = a.age_cit_id
-								LEFT JOIN paciente p ON c.cit_pac_id = p.pac_id
-								AND c.cit_activo = 1
-								WHERE a.age_activo = 1
-								AND a.age_cli_id = $locationId "
-					. " AND  (date(a.age_fecha_inicio) >= '" . $start . "' AND date(a.age_fecha_inicio) <= '" . $end . "') "
-					. $filter;
+				$start = date("Y-m-d");
+				$end = date("Y-m-d");
+				$filter = " AND a.age_usu_id = " . $idUser;
+				$RAW_QUERY = "SELECT a.age_id as id, a.age_fecha_inicio as start, a.age_fecha_fin as end, a.age_tipo_evento as tipo_evento,  p.pac_nombre, p.pac_seg_nombre, p.pac_apellido, p.pac_seg_apellido,p.pac_dui
+									FROM agenda a
+									LEFT JOIN cita c on c.cit_id = a.age_cit_id
+									LEFT JOIN paciente p ON c.cit_pac_id = p.pac_id
+									AND c.cit_activo = 1
+									WHERE a.age_activo = 1
+									AND a.age_cli_id = $locationId "
+						. " AND  (date(a.age_fecha_inicio) >= '" . $start . "' AND date(a.age_fecha_inicio) <= '" . $end . "') "
+						. $filter;
 
 
-			$statement = $em->getConnection()->prepare($RAW_QUERY);
-			$statement->execute();
-			$appointments = $statement->fetchAll();
-
+				$statement = $em->getConnection()->prepare($RAW_QUERY);
+				$statement->execute();
+				$appointments = $statement->fetchAll();
+			}else{
+				$appointments = array();
+			}
+			//$appointments = "";
 			$profilephoto = "";
 			$oProfileImage = $em->getRepository('AppBundle:UsuarioGaleria')->findOneBy( array("galUsuario"=>$idUser, "galCliente"=>$locationId, "galTipo"=>1) );
 			if( $oProfileImage )
@@ -89,18 +94,31 @@ class DashboardController extends Controller
 			//$em = $this->getDoctrine()->getManager();	
 
 			$unread = 0;
-			$oUnReadMessage = $em->getRepository('AppBundle:SolicitudContacto')->findBy( array("scUsuario"=>$idUser, "scCliente"=>$locationId, "estado"=>0) );
 			
-			if( count($oUnReadMessage) > 0 )
+			if( $locationId != "" )
 			{
-				$unread = count($oUnReadMessage);
+				$oUnReadMessage = $em->getRepository('AppBundle:SolicitudContacto')->findBy( array("scUsuario"=>$idUser, "scCliente"=>$locationId, "estado"=>0) );
+			
+				if( count($oUnReadMessage) > 0 )
+				{
+					$unread = count($oUnReadMessage);
+				}
+				
+				$RAW_QUERY = "SELECT * FROM solicitud_contacto WHERE sc_cli_id = $locationId AND sc_usu_id = $idUser ORDER BY fecha_contacto DESC LIMIT 20"; 
+				$statement = $em->getConnection()->prepare($RAW_QUERY);
+					//$statement->bindValue("idUser", $idUser );
+				$statement->execute();
+				$oMessages = $statement->fetchAll();
+				
+				exit();
 			}
+			else
+			{
+				$oMessages = array();
+			}
+			
 
-			$RAW_QUERY = "SELECT * FROM solicitud_contacto WHERE sc_cli_id = $locationId AND sc_usu_id = $idUser ORDER BY fecha_contacto DESC LIMIT 20"; 
-			$statement = $em->getConnection()->prepare($RAW_QUERY);
-				//$statement->bindValue("idUser", $idUser );
-			$statement->execute();
-			$oMessages = $statement->fetchAll();
+			
 		}
 
 		
