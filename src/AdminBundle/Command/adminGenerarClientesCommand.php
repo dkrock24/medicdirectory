7ebdaef011a75918ee20f29be1bc5289c3040dcc
Carlos Hidalgo <vladimiroski@gmail.com>
2017-07-28 08:55:46 -0600

    Rename & Update admin generation commands

--- src/AdminBundle/Command/adminGenerarClientesCommand.php
+++ src/AdminBundle/Command/adminGenerarClientesCommand.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 
-class adminGenerarClientesPruebaCommand extends ContainerAwareCommand {
+class adminGenerarClientesCommand extends ContainerAwareCommand {
 
     protected function configure() {
         /* php bin/console admin:generar-clientes-prueba */
