bd19b9c33e03c172cd70a9a5212170a00439ac6c
Vladimir Hidalgo <vladimiroski@gmail.com>
2017-08-30 23:46:02 -0600

    Fixes en landing + profile

--- src/WebBundle/Controller/DefaultController.php
+++ src/WebBundle/Controller/DefaultController.php
@@ -39,13 +39,15 @@ class DefaultController extends Controller {
         }
 
         // Raw Query
-        $RAW_QUERY  = "select *, group_concat(e.esp_especialidad SEPARATOR ', ') as especialidades from usuario u
-                        JOIN cliente_usuario as cu on cu.cli_usu_usu_id=u.usu_id
-                        JOIN usuario_especialidad AS es on u.usu_id=es.id_usuario
-                        JOIN  especialidad as e on e.esp_id=es.id_especialidad
-                        JOIN usuario_galeria as ug on ug.gal_usu_id=cu.cli_usu_usu_id
-                        WHERE ug.gal_modulo_id is null and ug.gal_tipo=1 and ug.gal_aprobado=1 and cu.cli_usu_rol_id=6 $sUsuarios
-                        group by u.usu_id";
+        $RAW_QUERY  = "SELECT *, group_concat(e.esp_especialidad SEPARATOR ', ') as especialidades from usuario u
+JOIN cliente_usuario as cu on cu.cli_usu_usu_id=u.usu_id
+JOIN cliente as c ON cu.cli_usu_cli_id = c.cli_id
+JOIN municipio as m ON c.cli_mun_id = m.mun_id
+JOIN usuario_especialidad AS es on u.usu_id=es.id_usuario
+JOIN  especialidad as e on e.esp_id=es.id_especialidad
+JOIN usuario_galeria as ug on ug.gal_usu_id=cu.cli_usu_usu_id
+WHERE ug.gal_modulo_id is null and ug.gal_tipo=1 and ug.gal_aprobado=1 and cu.cli_usu_rol_id=6 
+GROUP BY u.usu_id";
         $statement  = $em->getConnection()->prepare($RAW_QUERY);
         $statement->execute();    
         $medicos    = $statement->fetchAll();            
