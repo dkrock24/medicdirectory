5150272e4554371c11c2135cb0788d0913ecd4d7
carlos.hidalgo <carlos.hidalgo@telusinternational.com>
2017-09-05 23:04:50 -0600

    SphinxQL

--- src/Services/Busqueda.php
+++ src/Services/Busqueda.php
@@ -43,7 +43,7 @@ class Busqueda {
             $sphinxSearch->setFilter('cli_activo', array($activos), false);
         }
         $oResultado = $sphinxSearch->search($texto, $indexesToSearch, $options);
-        var_dump($oResultado);
+
         $aRet['total'] = $oResultado['total'];
         $aRet['total_encontrado'] = $oResultado['total_found'];
 
@@ -73,29 +73,31 @@ class Busqueda {
         // Buscamos en el indice definido en el config.yml -> SphinxSearch
         // La configuración del indice esta en el archivo sphinx.conf del sistema
         $indexesToSearch = array('usuarios');
-        $options = array(
-            'result_offset' => $offset,
-            'result_limit' => $limit,
-        );
-        $sphinxSearch = $this->sphinxsearch;
-        $sphinxSearch->setMatchMode(SPH_MATCH_EXTENDED);
+
 
         // Si nos pasaron un valor de estado activo entonces lo aplicamos como filtro
         // si viene null omitimos el filtro para buscar todos
-        if (null !== $activos) {
+        /*
+		if (null !== $activos) {
             $sphinxSearch->setFilter('cli_usu_activo', array($activos), false);
         }
-        $oResultado = $sphinxSearch->search(utf8_encode($texto), $indexesToSearch, $options);
-        $aRet['total'] = $oResultado['total'];
-        $aRet['total_encontrado'] = $oResultado['total_found'];
+		*/
 
-
-        // Si tuvimos resultados entonces devolver los IDs ( cliente -> cli_id )
-        if ($aRet['total'] > 0) {
-            foreach ($oResultado['matches'] as $id => $usuario) {
-                $aRet['ids'][] = $id;
-            }
+        $conn = new \mysqli(0, '', '', 'usuarios', 9306);
+        if ($conn->connect_error) {
+            throw new Exception('Connection Error: ['.$conn->connect_errno.'] '.$conn->connect_error, $conn->connect_errno);
         }
+
+        $resource = $conn->query("SELECT * FROM usuarios WHERE match('$texto')");
+
+        while ($row = $resource->fetch_assoc()) {
+            $aRet['ids'][] = $row['id'];
+        }
+
+        $aRet['total'] = $resource->num_rows;
+        $aRet['total_encontrado'] = $resource->num_rows;
+
+        $resource->free_result();
 
         return $aRet;
     }
